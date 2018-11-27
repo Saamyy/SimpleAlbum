f8a612dccfb4c5b9cd1a1b09053d868eea209536
mahmoud afifi <mahmoud.afifi@vodafone.com>
2018-11-27 19:47:47 +0200

    add the end points interface inside the network package

---
+++ app/src/main/java/com/example/mahmoudafifi/simplealbum/network/ApiRetrofit.java
@@ -0,0 +1,18 @@
+package com.example.mahmoudafifi.simplealbum.network;
+
+
+import com.example.mahmoudafifi.simplealbum.model.Image;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface ApiRetrofit {
+
+    @GET("/photos")
+    Call<List<Image>> getAllImages();
+    @GET("/photos")
+    Call<List<Image>> getImagesByAlbumId(@Query("albumId") String albumId);
+}
