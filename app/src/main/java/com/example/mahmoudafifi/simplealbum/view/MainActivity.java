92e573d83c993f22a88abd943fd5fe0d5c500281
mahmoud afifi <mahmoud.afifi@vodafone.com>
2018-11-27 20:31:19 +0200

    navigate to albumListFragment from the main Activity

--- app/src/main/java/com/example/mahmoudafifi/simplealbum/view/MainActivity.java
+++ app/src/main/java/com/example/mahmoudafifi/simplealbum/view/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.mahmoudafifi.simplealbum.R;
+import com.example.mahmoudafifi.simplealbum.view.albumList.AlbumListFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AlbumListFragment AlbumListFragment = new AlbumListFragment();
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.contentFrameLayout, AlbumListFragment)
+                .commit();
     }
 }
